6bfd6395d8eafcc1b0213cc313b6dee3f4d185ec
L-Asus <leninisaac444@gmail.com>
2025-02-02 21:45:55 -0500

    Migración a mongo

--- services/sembrio_service.py
+++ services/sembrio_service.py
@@ -1,15 +1,18 @@
-from config.firebase_config import db
+#from config.firebase_config import db
+from config.mongo_config import db
 
 def add_sembrio(data):
     try:
-        sembrio_ref = db.collection('sembríos').add(data)
+        #sembrio_ref = db.collection('sembríos').add(data)
+        sembrio_ref = db.sembrio.insert_one(data)
         return {'id': sembrio_ref[1].id}
     except Exception as e:
         return {'error': str(e)}
 
 def get_all_sembríos():
     try:
-        sembríos = [doc.to_dict() | {'id': doc.id} for doc in db.collection('sembríos').stream()]
+        #sembríos = [doc.to_dict() | {'id': doc.id} for doc in db.collection('sembríos').stream()]
+        sembríos = [doc.to_dict() | {'id': doc.get('_id')} for doc in list(db.sembrios.find())]
         return sembríos
     except Exception as e:
         return {'error': str(e)}
--- services/user_service.py
+++ services/user_service.py
@@ -1,24 +1,31 @@
-from config.firebase_config import db
+#from config.firebase_config import db
+from bson import ObjectId
+
+from config.mongo_config import db
 
 def create_user(data):
     try:
-        user_ref = db.collection('usuarios').add(data)
-        return {'id': user_ref[1].id}
+        #user_ref = db.collection('usuarios').add(data)
+        user_ref = db.usuarios.insert_one(data)
+        #return {'id': user_ref[1].id}
+        return {'id': user_ref[1].get('_id')}
     except Exception as e:
         return {'error': str(e)}
 
 def get_all_users():
     try:
-        users = [doc.to_dict() | {'id': doc.id} for doc in db.collection('usuarios').stream()]
+        # users = [doc.to_dict() | {'id': doc.id} for doc in db.collection('usuarios').stream()]
+        users = [doc.to_dict() | {'id': doc.get('_id')} for doc in db.usuarios.find()]
         return users
     except Exception as e:
         return {'error': str(e)}
 
 def get_user_by_id(user_id):
     try:
-        doc = db.collection('usuarios').document(user_id).get()
+        #doc = db.collection('usuarios').document(user_id).get()
+        doc = db.usuarios.find_one({'_id': ObjectId(user_id)})
         if doc.exists:
-            return doc.to_dict() | {'id': doc.id}
+            return doc.to_dict() | {'id': doc.get('_id')}
         return {'error': 'Usuario no encontrado'}
     except Exception as e:
         return {'error': str(e)}
